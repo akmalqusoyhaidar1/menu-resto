ac55f55abdda39b5a0776753dc6712dd1ee9e411
akmalqusoyhaidar1 <akmale2076@gmail.com>
2025-11-04 16:57:37 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useCallback } from 'react'
-import { supabase } from './supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 // Definisikan tipe untuk item menu agar kode lebih aman dan terstruktur
 interface MenuItem {
